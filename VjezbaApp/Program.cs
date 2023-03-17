a18c02573c4afb27e09c341d3ddefa570df316cc
DESKTOP-DG2UJNT\HPofDada <data02@gmail.com>
2023-03-17 14:06:13 +0100

    Pdf creation and saving.

--- VjezbaApp/Program.cs
+++ VjezbaApp/Program.cs
@@ -9,7 +9,7 @@ using System.Diagnostics;
 
 namespace VjezbaApp
 {
-    public class CreateFolder
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -21,12 +21,14 @@ namespace VjezbaApp
 
             //specify the path to and the name of the file
             string filePath = @"C:\Users\HPofDada\Documents\CeeSharpFriday\FileIO\1stFile.txt";
+            string filePathTwo = @"C:\Users\HPofDada\Documents\CeeSharpFriday\FileIO\2ndFile.txt";
 
             try
             {
                 //create file if it doesn't exist
                 if (!File.Exists(filePath))
                 {
+                    //write text to file
                     using (StreamWriter writer = File.CreateText(filePath))
                     {
                         writer.WriteLine("Let's write something");
@@ -41,6 +43,17 @@ namespace VjezbaApp
                     while ((text = reader.ReadLine()) != null)
                     { Console.WriteLine(text); }
                 }
+
+                //create 2nd file if it doesn't exist
+                if (!File.Exists(filePathTwo)) 
+                {
+                    File.Create(filePathTwo);
+                }
+
+                //read all text from 1st file, then write it to 2nd file
+                string transferContent = File.ReadAllText(filePath);
+                File.WriteAllText(filePathTwo, transferContent);
+
             }
             catch (Exception exc)
             {
